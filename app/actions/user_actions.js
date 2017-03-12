4fe14df9a1eed297fbd8365c50a89b12271b40cb
Yi Zhao <zhaoyi0113@gmail.com>
2017-03-12 18:18:59 +1100

    change user course history api

--- app/actions/user_actions.js
+++ app/actions/user_actions.js
@@ -14,7 +14,7 @@ export const getUserOrders = (token, index, number) => {
 }
 
 export const getUserHistoryCourse = (token, index, number) => {
-  return commonActions.requestGet(types.USER_COURSE_HTTP, '/nurse/extension/course/' + index + '/' + number, token)
+  return commonActions.requestGet(types.USER_COURSE_HTTP, '/nurse/extension/course/self/' + index + '/' + number, token)
 }
 
 export const sendSuggestionFeedback = (token, content) => {
